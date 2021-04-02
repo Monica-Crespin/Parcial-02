38924b33dee02306b2b1b6edad232cacd7a17085
Monica-Crespin <sophievallejo1@gmail.com>
2021-04-02 15:41:47 -0600

    Parcial finalizado

--- js/estilo.js
+++ js/estilo.js
@@ -35,3 +35,63 @@ function ocultarMenu(){
         degradado.style.display = 'none';
     }, 250);
 }
+
+//Galería
+//Variables
+const imgs = document.querySelectorAll('.img');
+const lightbox = document.querySelector('#lightbox');
+const btnExit = document.querySelector('#exit');
+const boxImg = document.querySelector('#boxImg');
+const imgLightbox = document.querySelector('#imgLightbox');
+const nombreImg = document.querySelector('#nombre');
+
+//Eventos
+imgs.forEach( img => {
+    img.addEventListener('click', seleccionarImg);
+})
+btnExit.addEventListener('click', ocultarLightbox);
+document.addEventListener('keydown', filtrarTecla);
+document.addEventListener('click', filtrarElemento);
+
+//Funciones
+function seleccionarImg(e){
+    const img = e.target.children[0];
+    const src = img.getAttribute('src');
+    const nombre = img.getAttribute('nombre');
+    mostrarLightbox(src, nombre);
+}
+
+function filtrarTecla(e){
+    e.keyCode === 27 && ocultarLightbox();
+}
+
+function filtrarElemento(e){
+    e.target.id === 'lightbox' && ocultarLightbox();
+}
+
+function mostrarLightbox(src, nombre){
+    //Animar el bloque de Lightbox
+    lightbox.style.display = 'flex';
+    setTimeout(()=>{
+        lightbox.style.opacity = '1';
+    }, 10);
+
+    //Animar el bloque de la imagen
+    setTimeout(()=>{
+        boxImg.style.opacity = '1';
+    }, 250);
+
+    //Asignar valores
+    imgLightbox.setAttribute('src', src);
+    nombreImg.textContent = `Nombre: ${nombre}`;
+}
+
+function ocultarLightbox(){
+    boxImg.style.opacity = '0';
+    setTimeout(()=>{
+        lightbox.style.opacity = '0';
+    }, 250);
+    setTimeout(()=>{
+        lightbox.style.display = 'none';
+    }, 500);
+}
